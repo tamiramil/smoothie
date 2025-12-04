b8f78957fa2348f764e6a0de3b0f8f9b93b703a1
tamiramil <tamiramil@proton.me>
2025-12-04 16:20:32 +0600

    fix(wizard): remove [Required] attributes

--- ViewModel/ProjectWizardViewModel.cs
+++ ViewModel/ProjectWizardViewModel.cs
@@ -6,30 +6,21 @@ namespace smoothie.ViewModel;
 
 public class ProjectWizardViewModel
 {
-    [Required(ErrorMessage = "Please enter a file name")]
     [StringLength(50, ErrorMessage = "The name length cannot exceed 50 characters")]
     public string? Name { get; set; }
 
-    [Required(ErrorMessage = "Please enter the start date")]
     [DataType(DataType.Date)]
     public DateTime? StartDate { get; set; }
 
-    [Required(ErrorMessage = "Please enter the end date")]
     [DataType(DataType.Date)]
     public DateTime? EndDate { get; set; }
 
-    [Required(ErrorMessage = "Please enter the priority")]
     [Range(1, 10, ErrorMessage = "The priority must be between 1 and 10")]
     public int? Priority { get; set; }
 
-    [Required(ErrorMessage = "Select suctomer company")]
     public int? CustomerCompanyId { get; set; }
-
-    [Required(ErrorMessage = "Select executor company")]
     public int? ExecutorCompanyId { get; set; }
-
-    [Required(ErrorMessage = "Please select the head of the project")]
-    public int? HeadId { get; set; }
+    public int? HeadId            { get; set; }
 
     public List<int>?       EmployeeIds       { get; set; } = new();
     public List<IFormFile>? UploadedFiles     { get; set; } = new();
